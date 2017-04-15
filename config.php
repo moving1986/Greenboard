71ad3d750244f2dbc200927f06d987d44e36dd13
admin <aleksei_mezencev@mail.ru>
2017-04-15 17:47:24 +0700

    Rejected commit: Default

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+define("DB_HOST",'localhost');
+define("DB_USER",'root');
+define("DB_PASSWORD",'');
+define("DB_NAME",'board'); 
+define("PREF",'mes_');
+define("SITE_NAME",'http://localhost/board');
+define("TEMPLATE",'template/default/');
+define("ACTIONS",'actions/');
+define("FILES",'files/mini/');
+define("IMG_WIDTH",400);
+
+define("PERPAGE",4);
+define("SITE_NAME_HEADER",'Green board - доска бесптаных объявлений');
