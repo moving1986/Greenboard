722531cfdc5d4043f1b162d2843902e8a4342eb8
admin <aleksei_mezencev@mail.ru>
2017-04-23 19:39:30 +0700

    Rejected commit: Default

--- actions/add_mess.php
+++ actions/add_mess.php
@@ -1,2 +1,18 @@
 <?php
-$content = render(TEMPLATE . "add_mess.tpl");
+if (!$user || !can($user['id_role'],array("add_mess"))) {
+    $text = "Доступ запрещен!";
+    $content = render(TEMPLATE . "error.tpl", array("text"=>"$text"));
+} else {
+    if($_POST) {
+        $msg = add_mess($_POST, $user['user_id']);
+        if($msg === TRUE) {
+            $_SESSION['msg'] = "Ваше объявление успешно добавлено и ожидает проверки модератором";
+        } else {
+            $_SESSION['msg'] = $msg;
+        }
+        header("Location:?action=add_mess");
+        exit();
+    }
+    $content = render(TEMPLATE . "add_mess.tpl", array('categories'=>$categories,
+        'razd'=>$razd));
+}
